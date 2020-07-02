7f258e5ad77ae3df75d08a37d957c16c72373a56
Annika Rytkonen <rytkonenannika@gmail.com>
2020-07-02 16:17:45 +0300

    Added register and authenticate user functions

--- app.js
+++ app.js
@@ -5,33 +5,42 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config = require('./config/database');
-const users = require('./routes/users');
 
 //Connect to database
-mongoose.connect(config.database);
+const config = require('./config/database');
 
-mongoose.connection.on('connected', () => {
-    console.log(`Connected to database ${config.database}`);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(config.database, {
+    useMongoClient: true
+}, (err) => {
+    if (err) {
+        console.log(`Database error: ${err}`);
+    } else {
+        console.log(`Successfully connected to ${config.database}`);
+    }
 });
 
-mongoose.connection.on('error', (err) => {
-    console.log(`Database error: ${err}`);
-})
-
-//Routers
-app.use('/users', users);
-
-//Middleware
-app.use(express.static(path.join(__dirname, 'public')));
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-//Homepage
+// Initialize and require passport
+app.use(passport.initialize());
+app.use(passport.session());
+require('./config/passport')(passport);
+
+// Users route
+const users = require('./routes/users');
+app.use('/users', users);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Homepage
 app.get('/', (req, res) => {
     res.send('Invalid Endpoint');
 });
 
-//Set up and listen on port
+// Set up and listen on port
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+// User Schema
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+// Get user by ID
+module.exports.getUserById = (id, callback) => {
+    User.findById(id, callback);
+};
+
+// Get user by username
+module.exports.getUserByUsername = (username, callback) => {
+    const query = {
+        username: username
+    }
+    User.findOne(query, callback);
+};
+
+// Add user
+module.exports.addUser = function (newUser, callback) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save(callback());
+        });
+    });
+};
+
+// Compare passwords
+module.exports.comparePassword = function (candidatePassword, hash, callback) {
+    bcrypt.compare(candidatePassword, hash, (err, isMatch) => {
+        if (err) throw err;
+        callback(null, isMatch)
+    });
+};
